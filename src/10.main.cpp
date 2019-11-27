ab7432d1fdc2fd1d2400402ec7c57be53f09f621
Oliv4945 <oliv4945@gmail.com>
2019-11-27 22:28:04 +0100

    Update to light when switch is pushed

--- src/10.main.cpp
+++ src/10.main.cpp
@@ -4,8 +4,6 @@
 
 #define BUTTON PC13
 
-const PinMap PinMap_DAC[] = {};
-
 void setup() {
   Serial.begin(115200);
   Serial.print("Start\n");
@@ -17,9 +15,13 @@ void setup() {
 
 void loop() {
   // Déclare et initialise des variables
-  int8_t coefficient = 50;
-  bool led_state = false;
+  bool button_state = false;
   while(1) {
-    digitalWrite(D13, digitalRead(BUTTON));
+    button_state = digitalRead(BUTTON);
+    if (button_state == true) {
+      digitalWrite(D13, false);
+    } else {
+      digitalWrite(D13, true);
+    }
   }
 }
